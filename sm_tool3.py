34806500d656b1e9e64a61a961b495f5b147e749
SupplyAnalytics <156051121+SupplyAnalytics@users.noreply.github.com>
2024-07-16 20:46:46 +0530

    Update sm_tool3.py

--- sm_tool3.py
+++ sm_tool3.py
@@ -1,25 +1,18 @@
 import streamlit as st
 import pandas as pd
-import gspread
 import datetime
-from google.oauth2 import service_account
+from PIL import Image
+from io import BytesIO
+import requests
 
-def export_data():
-    json_key_path = 'cred.json'
 
-    # Load credentials from the JSON key file
-    creds = service_account.Credentials.from_service_account_file(json_key_path, scopes=['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive'])
-    # Authorize the client using the credentials
-    client = gspread.authorize(creds)
-    # Open the Google Sheets document by title
-    source_sheet_title = 'Variant_Logging_Data_Base'
-    source_sheet = client.open(source_sheet_title)
-    # Select a specific worksheet within the source document
-    source_worksheet = source_sheet.worksheet('Logging Data Base') 
-    # Read existing data from the sheet
-    existing_data = source_worksheet.get_all_values()
-    sheet_df = pd.DataFrame(existing_data[1:], columns=existing_data[0])
-    return sheet_df
+def resize_image(url, width, height):
+    response = requests.get(url)
+    img = Image.open(BytesIO(response.content))
+    img = img.resize((width, height),Image.LANCZOS)
+    
+    return img
+
 
 def log_action(log_df, variant_id, status, df):
     current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -29,24 +22,22 @@ def log_action(log_df, variant_id, status, df):
     return log_df, df
 
 def image_gallery(df, log_df, result_dict, images, layer_name, start_index):
-    # Product details dictionary
     product_details = result_dict
-    # Number of columns
     num_columns = 8
     i = 0
 
-    # Display images and product details in a grid layout
     cols = st.columns(num_columns)
     for idx, img_url in enumerate(images):
-        col = cols[idx % num_columns]
-        col.image(img_url, width=80)
+        col = cols[idx % num_columns] 
+        # image = resize_image(img_url, width = 160, height = 160) 
+        col.image(img_url)
         if img_url in product_details:
             details = product_details[img_url]
             col.write(f"{details['Productname']}")
-            col.write(f"{details['Last30DayGMV']}")
+            # col.write(f"{details['Last30DayGMV']}")
             
             button_cols = col.columns(2)
-            forward_button = button_cols[0].button("++", key=f"{idx}_forward_{img_url}_{i}")
+            forward_button = button_cols[0].button(r"$\textsf{+}$", key=f"{idx}_forward_{img_url}_{i}")
             if forward_button:
                 log_df, df = log_action(log_df, details['VariantId'], 'Forwarded to seller', df)
                 images.remove(img_url)
@@ -54,7 +45,7 @@ def image_gallery(df, log_df, result_dict, images, layer_name, start_index):
                 st.session_state['log_df'] = log_df
                 df.to_csv('VLD.csv', index=False)
                 st.rerun()
-            remove_button = button_cols[1].button("--", key=f"{idx}_remove_{img_url}_{i}")
+            remove_button = button_cols[1].button(r"$\textsf{-}$", key=f"{idx}_remove_{img_url}_{i}", type="primary")
             if remove_button:
                 log_df, df = log_action(log_df, details['VariantId'], 'Removed', df)
                 images.remove(img_url)
@@ -65,9 +56,12 @@ def image_gallery(df, log_df, result_dict, images, layer_name, start_index):
             log_df.to_csv('Log_DF.csv', index=False)
             i = i + 1
 
-# df = export_data()
-df = pd.read_csv('VLD.csv')
+# Set up the page layout
+st.set_page_config(layout="wide")
 
+
+
+# Initialize session state variables if they don't exist
 if "red_index" not in st.session_state:
     st.session_state.red_index = 0
 if "yellow_index" not in st.session_state:
@@ -75,29 +69,60 @@ if "yellow_index" not in st.session_state:
 if "green_index" not in st.session_state:
     st.session_state.green_index = 0
 
-if "red_images" not in st.session_state:
-    st.session_state.red_images = df[df['Status'] == "Out of Stock"]['ImageUrl'].tolist()
-if "yellow_images" not in st.session_state:
-    st.session_state.yellow_images = df[df['Status'] == "Forwarded to seller"]['ImageUrl'].tolist()
-if "green_images" not in st.session_state:
-    st.session_state.green_images = []
+# Login logic
+if "logged_in" not in st.session_state:
+    st.session_state.logged_in = False
 
-if "log_df" not in st.session_state:
-    st.session_state.log_df = pd.DataFrame(columns=['VariantId', 'Status', 'Timestamp'])
 
-result_dict = {}
-for index, row in df.iterrows():
-    result_dict[row['ImageUrl']] = {
-        'VariantId': row['VariantId'],
-        'Productname': row['Productname'],
-        'Last30DayGMV': row['Last30DayGMV'],
-        'SM': row['SM']
-    }
 
-st.header("Out of Stock")
-image_gallery(df, st.session_state.log_df, result_dict, st.session_state.red_images, 'red', start_index=st.session_state.red_index)
 
-st.header("Forwarded to Seller")
-image_gallery(df, st.session_state.log_df, result_dict, st.session_state.yellow_images, 'yellow', start_index=st.session_state.yellow_index)
+# Check if the user is logged in
+if not st.session_state.logged_in:
+    # Login form
+    st.title("Login")
+    email = st.text_input("Email")
+    col1, col2, col3, col4 = st.columns([1, 1, 1, 1]) 
+    with col1:
+        Platform = st.selectbox("Select Platform", ["All", "Production", "Distribution", "BijnisExpress"], index=0)
+    with col2:
+        supercategory = st.selectbox("Select SuperCat", ["All", "Footwear", "Apparels"])
+    with col3:
+        subcategory = st.selectbox("Select Subcat", ["All"], index=0)
+    with col4:
+        price_ranges = st.slider("Select Price Range", 0, 5000, (0, 5000), step=50)
+    if st.button("Submit"):
+        if email:  # You can add more validation if needed
+            st.session_state.logged_in = True
+            st.rerun()
+else:
+    # Main application code
+    df = pd.read_csv('Inactive_variants_data.csv')
 
-st.header("Live Variants")
+    if "red_images" not in st.session_state:
+        st.session_state.red_images = df[df['Status'] == "Out of Stock"]['resizeUrl'].tolist()
+    if "yellow_images" not in st.session_state:
+        st.session_state.yellow_images = df[df['Status'] == "Forwarded to seller"]['resizeUrl'].tolist()
+    if "green_images" not in st.session_state:
+        st.session_state.green_images = []
+
+    if "log_df" not in st.session_state:
+        st.session_state.log_df = pd.DataFrame(columns=['VariantId', 'Status', 'Timestamp'])
+
+    result_dict = {}
+    for index, row in df.iterrows():
+        result_dict[row['resizeUrl']] = {
+            'VariantId': row['VariantId'],
+            'Productname': row['Productname'],
+            'Last30DayGMV': row['Last30DayGMV'],
+            'SM': row['SM']
+        }
+
+    
+    with st.container(border = True):
+        st.header("Out of Stock")
+        image_gallery(df, st.session_state.log_df, result_dict, st.session_state.red_images, 'red', start_index=st.session_state.red_index)
+    with st.container(border = True):
+        st.header("Forwarded to Seller")
+        image_gallery(df, st.session_state.log_df, result_dict, st.session_state.yellow_images, 'yellow', start_index=st.session_state.yellow_index)
+    with st.container(border = True):
+        st.header("Live Variants")
